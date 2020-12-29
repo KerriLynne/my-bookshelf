fe223e3873574354fbe3ba3ea9071d4a765e4d5a
KerriLynne <kerri.l.ogorman@gmail.com>
2020-12-29 13:12:45 -0500

    finished form, added search feature

--- frontend/src/components/BookForm.js
+++ frontend/src/components/BookForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { createBook, showBook, updateBook } from '../redux/actions/bookActions';
 import { connect } from 'react-redux'
-import {reduxForm} from 'redux-form';
+// import {reduxForm} from 'redux-form';
 
 class BookForm extends Component {
 
@@ -12,57 +12,52 @@ class BookForm extends Component {
         }
     }
 
-
-    state = {
-        title: "",
-        author: "",
-        genre: "",
-    };
-
     isAdding = !this.props.match.params.id
 
-    submit = (e, values) => {
+    submit = (e) => {
         e.preventDefault()
-        console.log(values)
+        const values = {
+            title: this.bookTitle.value,
+            author: this.bookAuthor.value,
+            genre: this.bookGenre.value
+        }
         if (this.isAdding) {
             this.props.createBook(values)
-            // this.props.history.push('/books');
+            this.props.history.push('/books');
         } else {
             this.props.updateBook(this.props.match.params.id, values) //first argument is the :id in the url, second argument is the params
             this.props.history.push(`/books/${this.props.match.params.id}`);
         }
-        this.setState({
-            title: "",
-            author: "",
-            genre: "",
-        });
     };
 
     render() {
-        const {fields: {title, author, genre}} = this.props;
         return (
-            <div>
+            <div key={this.props.match.params.id}>
                 <h1 className="title">{this.isAdding ? "Add" : "Edit" } a book</h1>
-                <form onSubmit={this.submit}>
+                <form onSubmit={this.submit.bind(this)}>
                     Title:{" "} 
                     <input 
-                    // onChange={e => this.setState({title: e.target.value})}
+                    ref={(input) => this.bookTitle = input}
+                    //storing a reference to that formfield
+                    defaultValue={this.props.book && this.props.book.title}
                     type="text" 
-                    {...title}
-                    // value={this.props.book && this.props.book.title} 
                     /> 
                     Author:{" "} 
                     <input 
+                    ref={(input) => this.bookAuthor = input}
+                    defaultValue={this.props.book && this.props.book.author}
                     // onChange={e => this.setState({author: e.target.value})}
                     type="text" 
-                    {...author}
+                    // {...author}
                     // value={this.props.book && this.props.book.author} 
                     /> 
                     Genre:{" "} 
                     <input 
+                    ref={(input) => this.bookGenre = input}
+                    defaultValue={this.props.book && this.props.book.genre}
                     // onChange={e => this.setState({genre: e.target.value})}
                     type="text" 
-                    {...genre}
+                    // {...genre}
                     // value={this.props.book && this.props.book.genre} 
                     />     
                     <input type="submit" value={this.isAdding ? "Add" : "Update" } />
@@ -72,10 +67,10 @@ class BookForm extends Component {
     }
 }
 
-BookForm = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
-    form: 'book',                           // a unique name for this form
-    fields: ['title', 'author', 'genre'] // all the fields in your form
-  })(BookForm);
+// BookForm = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
+//     form: 'book',                           // a unique name for this form
+//     fields: ['title', 'author', 'genre'] // all the fields in your form
+//   })(BookForm);
 
 const mapStateToProps = ({ books }) => {
     //destructiure books and return an object where book is the key but books.select is the value 
--- frontend/src/components/BookIndex.js
+++ frontend/src/components/BookIndex.js
@@ -13,12 +13,30 @@ class BookIndex extends React.Component {
     componentDidMount() {
         this.props.getBooks();
     }
+
+    state = {
+        titleSearch: ""
+    };
+
+    filterBooks(book) {
+        if (!this.state || this.state.titleSearch === "") {
+            return true
+        }
+        return (
+            book.title.toLowerCase().match(this.state.titleSearch.toLowerCase())
+        )
+    }
+
     render() {
         console.log(this.props.books)
         return (
             <div>
                 <h1 className="title">Books</h1>
-                {this.props.books.map((book) => (
+                <input
+                type="text"
+                onChange={e => this.setState({titleSearch: e.target.value})}/>
+
+                {this.props.books.filter(this.filterBooks.bind(this)).map((book) => (
                 <BookListItem key={book.id} book={book} />
                 ))}
             </div>
