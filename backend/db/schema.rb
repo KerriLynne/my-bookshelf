f08070f98831e05b490e46bda0601f2442ca0f8a
KerriLynne <kerri.l.ogorman@gmail.com>
2021-04-14 14:07:38 -0400

    added rating column to books

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_162636) do
+ActiveRecord::Schema.define(version: 2021_04_14_175904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_162636) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "bookshelf_id"
+    t.integer "rating"
   end
 
   create_table "bookshelves", force: :cascade do |t|
